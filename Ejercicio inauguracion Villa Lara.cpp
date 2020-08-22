3e3932d23d155edb27ca3bdf4dc5b64549d65a00
MauriUTN <70033943+MauriUTN@users.noreply.github.com>
2020-08-21 21:13:22 -0300

    Add files via upload

---
+++ Ejercicio inauguracion Villa Lara.cpp
@@ -0,0 +1,47 @@
+/* Una fábrica de caramelos dispone de un presupuesto inicial para inaugurar su sucursal en Villa Brian Lara. Se sabe que para producir caramelos tienen los siguientes costos:
+Costo de alquiler de $10000
+Costo por caramelo producido de $0.5
+Costo por mantenimiento cada 1000 caramelos de $5000
+
+Dados el presupuesto inicial y la cantidad de caramelos a producir el primer mes, informar:
+"El presupuesto es suficiente para cubrir los costos de $XXXX"
+"El presupuesto no es suficiente, necesita un crédito de $XXXX"
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+   float presupuesto;
+   float alquiler;
+   float mantenimiento = 0;
+   float costo_total;
+   int caramelos;
+
+   cout << "Presupuesto: $";
+   cin >> presupuesto;
+   cout << " Caramelos a producir: ";
+   cin >> caramelos;
+
+   mantenimiento = (caramelos/1000)*500;
+   costo_total = alquiler + (caramelos*0.5) + mantenimiento;
+
+   cout << endl << endl;
+   if (presupuesto >= costo_total){
+    cout << "El presupuesto es suficiente para cubrir los costos de $" << costo_total - presupuesto;
+
+   }
+   else{
+        cout << "El presupuesto no es suficiente, necesita un crédito de $";
+
+   }
+
+
+
+
+
+
+
+return 0;
+
+
+}
---
+++ Proyecto Granja.cpp
@@ -0,0 +1,145 @@
+/* Una granja dispone de 45 conejos blancos, 50 conejos negros, 40 marrones y 49 grises. Se pide hacer un programa donde se ingresen tres ventas. Cada venta está compuesta por:
+Cantidad de conejos
+Tipo de conejo (1 - Blanco, 2 - Negro, 3- Marrón, 4 - Gris)
+
+Calcular e informar:
+Cantidad total de conejos vendidos.
+Cuantos conejos quedaron de cada tipo.
+Los tipos de conejos que no se vendieron.
+
+NOTA: Ninguna venta superará los 10 conejos*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+     int cantidad, tipo, cantidadTotal=0;
+     int cMar = 40, cBla = 45, cNeg = 50, cGri = 49;
+     bool bNeg, bBla, bGri, bMar;
+     bNeg = bBla = bGri = bMar = false;
+
+
+     // venta #1
+     cout << "Cantidad: ";
+     cin >> cantidad;
+
+     cout << "Tipo: ";
+     cin >> tipo;
+
+     cantidadTotal += cantidad;
+
+
+     switch (tipo)
+    {
+    case 1:
+        cBla -= cantidad;
+        bBla = true;
+        break;
+    case 2:
+        cMar -= cantidad;
+        bMar = true;
+        break;
+    case 3:
+        cNeg -= cantidad;
+        bNeg = true;
+        break;
+    case 4:
+        cGri -= cantidad;
+        bGri = true;
+        break;
+    }
+
+    // venta #2
+     cout << "Cantidad: ";
+     cin >> cantidad;
+
+     cout << "Tipo: ";
+     cin >> tipo;
+
+     cantidadTotal += cantidad;
+
+     switch (tipo)
+    {
+    case 1:
+        cBla -= cantidad;
+        bBla = true;
+        break;
+    case 2:
+        cMar -= cantidad;
+        bMar = true;
+        break;
+    case 3:
+        cNeg -= cantidad;
+        bNeg = true;
+        break;
+    case 4:
+        cGri -= cantidad;
+        bGri = true;
+        break;
+    }
+
+    // venta #3
+     cout << "Cantidad: ";
+     cin >> cantidad;
+
+     cout << "Tipo: ";
+     cin >> tipo;
+
+     cantidadTotal += cantidad;
+
+     switch (tipo)
+    {
+    case 1:
+        cBla -= cantidad;
+        bBla = true;
+        break;
+    case 2:
+        cMar -= cantidad;
+        bMar = true;
+        break;
+    case 3:
+        cNeg -= cantidad;
+        bNeg = true;
+        break;
+    case 4:
+        cGri -= cantidad;
+        bGri = true;
+        break;
+    }
+
+    // A
+
+    cout << "Cantidad total de conejos venidos: " << cantidadTotal << endl;
+
+    // B
+
+    cout << "Cuantos conejos quedaron de cada tipo: " << endl;
+    cout << " Blancos: " << cBla << endl;
+    cout << "Negros: " << cNeg << endl;
+    cout << "Marrones: " << cMar << endl;
+    cout << "Grises: " << cGri << endl;
+
+    //C
+
+    if (bBla == false) {
+        cout << "No se vendieron conejos blancos.";
+    }
+
+    if (bNeg == false) {
+        cout << "No se vendieron conejos negros.";
+    }
+
+    if (bMar == false) {
+        cout << "No se vendieron conejos marrones.";
+    }
+
+    if (cGri = false) {
+        cout << "No se vendieron conejos grises.";
+
+    }
+
+
+return 0;
+
+
+}
